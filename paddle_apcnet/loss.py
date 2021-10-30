6f0a616d663901dcc48429f8618e3eabae3a424f
haitaowu <1791360085@qq.com>
2021-10-30 17:46:12 +0800

    update diff

---
+++ paddle_apcnet/loss.py
@@ -0,0 +1,3 @@
+#CELOSS
+#  paddle.nn.CrossEntropyLoss(ignore_index=255, reduction='mean', axis=1)
+#  torch.nn.CrossEntropyLoss(ignore_index=255, reduction='mean', axis=1)
---
+++ paddle_apcnet/optimizer.py
@@ -0,0 +1,2 @@
+#torch.optim.SGD(model.parameters(), lr = 0.01, momentum=0.9,weight_decay=0.0005)
+#paddle.optimizer.Momentum(model.parameters(), lr = 0.01, momentum=0.9,weight_decay=0.0005)
